06e559e326f65d5f71d0c8a962b432deeeba3470
Ahmet Salih KARAGÖZ <krgzahmetsalih@gmail.com>
2023-08-23 23:49:21 +0300

    certificates uploaded, pages and json files added

---
+++ components/certificates/CertificateCard.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+
+function CertificateCard({ key, name, url }) {
+  return (
+    <div className="border items-center duration-300 rounded-xl overflow-hidden hover:shadow-xl transform hover:-translate-y-2">
+      <Image
+        key={key}
+        alt={name}
+        src={url}
+        width={300}
+        height={300}
+        className="w-full max-h-[32rem]"
+      />
+      <div className="p-3">
+        <h2 className="text-xl font-bold text-zinc-800 mt-1">{name}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default CertificateCard;
--- pages/certificates.js
+++ pages/certificates.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import CertificateCard from "@/components/certificates/CertificateCard";
+import useTranslation from "next-translate/useTranslation";
+import Image from "next/image";
+import React, { useState, useEffect } from "react";
 
 function Certificates() {
+  const { t } = useTranslation("certificates");
+  const [certificates, setCertificates] = useState([]);
+
+  useEffect(() => {
+    setCertificates(t("allCertficates", {}, { returnObjects: true }));
+    console.log(certificates);
+  }, [t]);
+
   return (
-    <div>Certificates</div>
-  )
+    <main className="flex flex-col min-h-screen min-[2000px]:w-[60%] max-lg:w-[95%] w-[75%] m-auto max-[1300px]:w-[95%]">
+      <div className="my-44 m-auto items-center grid grid-cols-3 gap-4">
+        {certificates &&
+          certificates.map((certificate, index) => {
+            return (
+              <CertificateCard
+                key={index}
+                name={certificate.name}
+                url={certificate.url}
+              />
+            );
+          })}
+      </div>
+    </main>
+  );
 }
 
-export default Certificates
+export default Certificates;
