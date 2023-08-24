8ec75d1cf9efcb8440402b8959c90ef305c55b1c
Ahmet Salih KARAGÖZ <krgzahmetsalih@gmail.com>
2023-08-24 22:29:50 +0300

    reference new logos added

--- components/SubHeader.jsx
+++ components/SubHeader.jsx
@@ -12,7 +12,7 @@ function SubHeader() {
 
   return (
     <InviewAnimate bezier={bezier} translate="translateY(300px)">
-      <section className="grid grid-cols-4 max-lg:grid-cols-1 w-[75%] min-[2000px]:w-[60%] max-[1300px]:w-[95%] max-lg:w-[90%] m-auto mt-28 max-lg:mt-12  rounded-4xl mb-12 max-lg:mb-0 text-white bg-[url('/images/footerImg.jpg')] bg-cover"> 
+      <section className="grid grid-cols-4 max-lg:grid-cols-1 w-[75%] min-[2000px]:w-[60%] max-[1300px]:w-[95%] max-lg:w-[90%] m-auto mt-28 max-lg:mt-12  rounded-4xl mb-12 max-lg:mb-0 text-white bg-[url('/images/footerImg.jpg')] bg-cover">
         <div className="bg-lightrans max-lg:bg-darkred rounded-l-4xl max-lg:rounded-bl-none max-lg:rounded-t-4xl px-7 py-12 flex flex-col items-center">
           <IoBulbSharp size={50} className="mb-5" />
           <h3 className="text-center mb-3 text-2xl font-medium">
